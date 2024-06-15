2f22bfaedb858fa7a785f2392a35e87edc4740fb
White12352 <86940955+White12352@users.noreply.github.com>
2024-06-15 17:25:21 +0800

    Update core.sh

--- buildScript/lib/core.sh
+++ buildScript/lib/core.sh
@@ -18,8 +18,9 @@ git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 cd jisd/libcore
 awk '{if(index($0, "// replace github.com/sagernet/sing =>") > 0) $0 = "replace github.com/sagernet/sing => ../../sing"}1' go.mod > temp_file && mv -f temp_file go.mod
-awk '{if ($0 ~ /\/\/ replace github\.com\/sagernet\/sing-dns => \.\.\/\.\.\/sing-dns/) {print "// replace github.com/sagernet/sing-dns => ../../sing-dns"; print ""; print "replace github.com/sagernet/sing-quic => ../../sing-quic"} else {print $0}}' go.mod > temp_file && mv -f temp_file go.mod
+#awk '{if ($0 ~ /\/\/ replace github\.com\/sagernet\/sing-dns => \.\.\/\.\.\/sing-dns/) {print "// replace github.com/sagernet/sing-dns => ../../sing-dns"; print ""; print "replace github.com/sagernet/sing-quic => ../../sing-quic"} else {print $0}}' go.mod > temp_file && mv -f temp_file go.mod
 awk '{if(index($0, "replace github.com/sagernet/sing-box =>") > 0) $0 = "replace github.com/sagernet/sing-box => ../../sing-box"}1' go.mod > temp_file && mv -f temp_file go.mod
+awk '{if(index($0, "replace github.com/sagernet/sing-quic =>") > 0) $0 = "replace github.com/sagernet/sing-quic => ../../sing-quic"}1' go.mod > temp_file && mv -f temp_file go.mod
 go mod tidy
 cd ..
 buildScript/lib/core/build.sh
