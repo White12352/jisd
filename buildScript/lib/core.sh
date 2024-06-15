ceeb1394c8acbbdead394709b1761c13e70e4aa0
White12352 <86940955+White12352@users.noreply.github.com>
2024-06-15 16:41:03 +0800

    Update core.sh

--- buildScript/lib/core.sh
+++ buildScript/lib/core.sh
@@ -2,6 +2,7 @@
 
 buildScript/lib/core/init.sh
 cd ..
+ls -la
 rm -rf sing-box
 #git clone -b building https://github.com/PuerNya/sing-box.git sing-box
 #git clone -b dev-next-yaott https://github.com/CHIZI-0618/sing-box.git sing-box
